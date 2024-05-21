726cf8ee2daec03a29bb7dd96a7a4d4ad1fa1938
alvitorzq <rizqisobri@gmail.com>
2024-05-21 16:18:47 +0700

    8.enhancing the app

--- controllers/error.js
+++ controllers/error.js
@@ -1,3 +1,6 @@
 exports.get404page = (req, res, next) => {
-   res.status(404).render('404', {pageTitle: 'Page Not Found'});
+   res.status(404).render('404', {
+      pageTitle: 'Page Not Found',
+      path: '/404'
+   });
 };
